1fb5671e2f84607b0e62a8613b4c207f6a4d3483
XessiveObserver <xessiveobserver@gmail.com>
2021-08-31 16:30:32 +0300

    navigation bar.

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { NavBar } from '../NavBar/NavBar';
 import { Api } from '../Api/Api';
+import { About } from '../Pages/About';
+import { Events } from '../Pages/Events';
 
 export const Routes = () => {
     return (
@@ -9,9 +11,15 @@ export const Routes = () => {
             <BrowserRouter>
                 <NavBar />
                 <Switch>  
-                    <Route exact path="/"><Api /></Route>
-                    <Route exact path="/about" />
-                    <Route exact path="/events" />
+                    <Route exact path="/">
+                        <Api />
+                    </Route>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                    <Route exact path="/events">
+                        <Events />
+                    </Route>
                 </Switch>
             </BrowserRouter>
         </div>
