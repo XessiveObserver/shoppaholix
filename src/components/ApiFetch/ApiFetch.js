1625956c8a14a41785c3cb4b7e2e9b6b21baf3db
XessiveObserver <xessiveobserver@gmail.com>
2021-10-21 16:17:48 +0300

    api link.

--- src/components/ApiFetch/ApiFetch.js
+++ src/components/ApiFetch/ApiFetch.js
@@ -1,4 +1,4 @@
 export function getList() {
-    return fetch('https://shoppaholix.herokuapp.com/api/v1/products/')
+    return fetch('http://shoppaholix.herokuapp.com/api/v1/products/')
         .then(data => data.json())
 }
