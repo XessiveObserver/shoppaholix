1ce0a6aa19d9a942c7bd592950c6c5eb8101e8f4
XessiveObserver <xessiveobserver@gmail.com>
2021-11-05 10:11:37 +0300

    hero-image, index-page.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,6 @@ import { NavBar } from '../NavBar/NavBar';
 import { Shop } from '../Pages/Shop/Shop';
 import { About } from '../Pages/About/About';
 import { Events } from '../Pages/Events/Events';
-import { HomeBoxes } from '../Pages/Home/HomeBoxes';
-import { HomeBoxe } from '../Pages/Home/HomeBox';
 import { HomeBoxesMore } from '../Pages/Home/HomeBoxesMore';
 import { Hero } from '../Hero/Hero';
 import { Footer } from '../Footer/Footer';
@@ -21,8 +19,6 @@ function App() {
                 <Switch>
                     <Route exact path="/" component={App}>
                         <Hero />
-                        <HomeBoxes />
-                        <HomeBoxe />
                         <HomeBoxesMore />
                     </Route>
                     <Route path="/about" component={About}>    
