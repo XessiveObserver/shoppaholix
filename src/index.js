47a68ab7dbeae503897367449091e049f00f2727
XessiveObserver <xessiveobserver@gmail.com>
2021-12-10 16:28:24 +0300

    redux provider removal..

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
